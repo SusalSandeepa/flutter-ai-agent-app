a3382bd3c6e31257257cbd14ae6e753446b23350
SusalSandeepa <susalsandeepa2002@gmail.com>
2026-05-16 20:56:59 +0530

    feat: add onboarding screen, model, and Lottie animation asset

---
+++ lib/model/onboard.dart
@@ -0,0 +1,13 @@
+class Onboard {
+  final String title, subtitle, lottie;
+  final double scale;
+
+  const Onboard({
+    required this.title,
+    required this.subtitle,
+    required this.lottie,
+    this.scale = 1.0,
+  });
+}
+
+// List
--- lib/screen/onboarding_screen.dart
+++ lib/screen/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ai_agent_app/model/onboard.dart';
+import 'package:flutter_ai_agent_app/screen/home_screen.dart';
 import 'package:lottie/lottie.dart';
 
 import '../helper/global.dart';
@@ -8,78 +10,113 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final c = PageController();
+    final list = [
+      Onboard(
+        title: 'Ask me anything',
+        subtitle: 'Get intelligent responses to your questions',
+        lottie: 'ai_ask_me',
+        scale: 2.5,
+      ),
+      Onboard(
+        title: 'Imagination to reality',
+        subtitle: 'Let your imagination run wild and watch it come to life.',
+        lottie: 'ai_play',
+        scale: 1.0,
+      ),
+    ];
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: mq.height * .6,
-            child: Transform.scale(
-              scale: 2.5,
-              child: Lottie.asset('assets/lottie/ai_ask_me.json'),
-            ),
-          ),
-          Text(
-            'Ask me anything',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w900,
-              letterSpacing: .5,
-            ),
-          ),
-
-          SizedBox(height: mq.height * .015),
-
-          SizedBox(
-            width: mq.width * .7,
-            child: Text(
-              'Get intelligent responses to your questions',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.black54,
-                fontWeight: FontWeight.w400,
-                letterSpacing: .5,
-              ),
-            ),
-          ),
-
-          const Spacer(),
-
-          //dots
-          Wrap(
-            spacing: 10,
-            children: List.generate(
-              2,
-              (i) => Container(
-                height: 8,
-                width: 10,
-                decoration: const BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.all(Radius.circular(5)),
+      body: PageView.builder(
+        controller: c,
+        itemCount: list.length,
+        itemBuilder: (ctx, ind) {
+          final isLast = ind == list.length - 1;
+          return Column(
+            children: [
+              SizedBox(
+                height: mq.height * .6,
+                child: Transform.scale(
+                  scale: list[ind].scale,
+                  child: Lottie.asset('assets/lottie/${list[ind].lottie}.json'),
                 ),
               ),
-            ),
-          ),
-
-          const Spacer(),
-
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: const StadiumBorder(),
-              elevation: 0,
-              textStyle: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: Colors.white,
+              Text(
+                list[ind].title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w900,
+                  letterSpacing: .5,
+                ),
               ),
-              minimumSize: Size(mq.width * .3, 40),
-              backgroundColor: const Color.fromARGB(255, 0, 140, 255),
-            ),
-            onPressed: () {},
-            child: const Text('Next', style: TextStyle(color: Colors.white)),
-          ),
 
-          const Spacer(flex: 2),
-        ],
+              SizedBox(height: mq.height * .015),
+
+              SizedBox(
+                width: mq.width * .7,
+                child: Text(
+                  list[ind].subtitle,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: .5,
+                  ),
+                ),
+              ),
+
+              const Spacer(),
+
+              //dots
+              Wrap(
+                spacing: 10,
+                children: List.generate(
+                  list.length,
+                  (i) => Container(
+                    height: 8,
+                    width: i == ind ? 15 : 10,
+                    decoration: BoxDecoration(
+                      color: i == ind ? Colors.blue : Colors.grey,
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                    ),
+                  ),
+                ),
+              ),
+
+              const Spacer(),
+
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                  elevation: 0,
+                  textStyle: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                  minimumSize: Size(mq.width * .3, 40),
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  if (isLast) {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (_) => HomeScreen()),
+                    );
+                  } else {
+                    c.nextPage(
+                      duration: Duration(milliseconds: 600),
+                      curve: Curves.ease,
+                    );
+                  }
+                },
+                child: Text(isLast ? 'Finish' : 'Next'),
+              ),
+
+              const Spacer(flex: 2),
+            ],
+          );
+        },
       ),
     );
   }
